ca3347ea656e93e05a2617b0c611d947ab588dd1
Eric Squires <eric.g.squires@gmail.com>
2016-09-13 22:03:28 -0400

    add controllers documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -37,7 +37,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
+#'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'numpydoc',
 ]
